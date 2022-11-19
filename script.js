8bdd63a8e01491546d475d6fe571468f7c519dc2
avetisyangayane1787 <117987537+avetisyangayane1787@users.noreply.github.com>
2022-11-19 13:55:50 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+//1
+
+let a = 14
+let b = 5
+
+alert(a+b)
+alert(a-b)
+alert(a/b)
+alert(a*b)
+alert(a%b)
+
+//2
+
+let name = "Gayane";
+let age = 18;
+let helo = 'My name is ${name}, I am ${age} years old.'
+console.log(hello);
+
+//3
+
+let number = prompt("Enter a number");
+if(number < 0) {
+ alert("yes");
+}
+else{
+    alert("no");
+}
+
+//4
+
+let number = prompt('enter your number');
+if(number % 2 === 0) {
+    alert(even);
+}
+else{
+    alert(odd);
+}
+
+//5
+
+let number1 = prompt("Enter a number ");
+let number2 = prompt("Enter a number ");
+if (number1 % number2 === 0 || number2 % number1 === 0) {
+    console.log(1)
+}
+else {
+    console.log(0)
+}
